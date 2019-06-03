e24703b46d005e2e9a53ef5f5fcc66603328fcfd
Hylianux <walkingspeedbump@gmail.com>
2019-06-03 15:23:18 -0400

    fixed url relativity the spoiler.css and spoiler.js now acknowledge the base url of the blog

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ hexo.extend.generator.register('spoiler_asset', () => [
 ]);
 
 hexo.extend.filter.register('after_post_render', (data) => {
-    var link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">";
-    var link_js = "<script src=\"/js/spoiler.js\" type=\"text/javascript\" async></script>";
+    var link_css = "<link rel=\"stylesheet\" href=\""+hexo.config.root+"css/spoiler.css\" type=\"text/css\">";
+    var link_js = "<script src=\""+hexo.config.root+"js/spoiler.js\" type=\"text/javascript\" async></script>";
     data.content += link_css + link_js;
     return data;
 });
