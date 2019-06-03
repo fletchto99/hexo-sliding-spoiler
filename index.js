d63528966d3fc0bc6c9ec76f6c5bc75055af6dcb
Hylianux <walkingspeedbump@gmail.com>
2019-06-03 15:40:03 -0400

    Update index.js
    Co-Authored-By: Matthew Langlois <fletchto99@gmail.com>

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ hexo.extend.generator.register('spoiler_asset', () => [
 ]);
 
 hexo.extend.filter.register('after_post_render', (data) => {
-    var link_css = "<link rel=\"stylesheet\" href=\""+hexo.config.root+"css/spoiler.css\" type=\"text/css\">";
+    let link_css = `<link rel="stylesheet" href="${hexo.config.root}css/spoiler.css" type="text/css">`;
     let link_js = `<script src="${hexo.config.root}js/spoiler.js" type="text/javascript" async></script>`;
     data.content += link_css + link_js;
     return data;
