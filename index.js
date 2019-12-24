831513dfeafebb6e3d6a713fdbd4b96961c0308d
Matt Langlois <fletchto99@github.com>
2019-12-23 23:20:19 -0500

    Relative to root

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const path = require('path');
 
 hexo.extend.tag.register('spoiler', (args, content) =>
 `<div class='spoiler collapsed'>
-  <div class='spoiler-title'>
-    ${args.join(" ")}
-  </div>
-  <div class='spoiler-content'>
-    ${
-      hexo.render.renderSync({
-        text: content,
-        engine: "markdown"
-      }) || "No content to show"
-    }
-  </div>
+    <div class='spoiler-title'>
+        ${args.join(" ")}
+    </div>
+    <div class='spoiler-content'>
+        ${
+            hexo.render.renderSync({
+                text: content,
+                engine: "markdown"
+            }) || "No content to show"
+        }
+    </div>
 </div>`, {
-  ends: true
+    ends: true
 });
 
 hexo.extend.generator.register('spoiler_asset', () => [
-  {
-    path: 'css/spoiler.css',
-    data: function () {
-      return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.css'));
+    {
+        path: 'css/spoiler.css',
+        data: function () {
+            return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.css'));
+        }
+    },
+    {
+        path: 'js/spoiler.js',
+        data: function () {
+            return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.js'));
+        }
     }
-  },
-  {
-    path: 'js/spoiler.js',
-    data: function () {
-      return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.js'));
-    }
-  }
 ]);
 
 hexo.extend.filter.register('after_post_render', (data) => {
-  let link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">";
-  let link_js = "<script src=\"/js/spoiler.js\" type=\"text/javascript\" async></script>";
-  data.content += link_css + link_js;
-  return data;
+    let link_css = `<link rel="stylesheet" href="${hexo.config.root}css/spoiler.css" type="text/css">`;
+    let link_js = `<script src="${hexo.config.root}js/spoiler.js" type="text/javascript" async></script>`;
+    data.content += link_css + link_js;
+    return data;
 });
