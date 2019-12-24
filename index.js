aa29d27cb49ea1cce06bf14eb2b7d18f7d8ab40a
Matt Langlois <fletchto99@github.com>
2019-12-23 21:34:18 -0500

    Formatting

--- index.js
+++ index.js
@@ -3,37 +3,39 @@ const path = require('path');
 
 hexo.extend.tag.register('spoiler', (args, content) =>
 `<div class='spoiler collapsed'>
-    <div class='spoiler-title'>
-        ${args.join(" ")}
-    </div>
-    <div class='spoiler-content'>
-        ${hexo.render.renderSync({
-            text: content,
-            engine: "markdown"
-        })}
-    </div>
+  <div class='spoiler-title'>
+    ${args.join(" ")}
+  </div>
+  <div class='spoiler-content'>
+    ${
+      hexo.render.renderSync({
+        text: content,
+        engine: "markdown"
+      }) || "No content to show"
+    }
+  </div>
 </div>`, {
-    ends: true
+  ends: true
 });
 
 hexo.extend.generator.register('spoiler_asset', () => [
-    {
-        path: 'css/spoiler.css',
-        data: function () {
-            return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.css'));
-        }
-    },
-    {
-        path: 'js/spoiler.js',
-        data: function () {
-            return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.js'));
-        }
+  {
+    path: 'css/spoiler.css',
+    data: function () {
+      return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.css'));
     }
+  },
+  {
+    path: 'js/spoiler.js',
+    data: function () {
+      return fs.createReadStream(path.resolve(path.resolve(__dirname, "./assets"), 'spoiler.js'));
+    }
+  }
 ]);
 
 hexo.extend.filter.register('after_post_render', (data) => {
-    let link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">";
-    let link_js = "<script src=\"/js/spoiler.js\" type=\"text/javascript\" async></script>";
-    data.content += link_css + link_js;
-    return data;
+  let link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">";
+  let link_js = "<script src=\"/js/spoiler.js\" type=\"text/javascript\" async></script>";
+  data.content += link_css + link_js;
+  return data;
 });
